81b570ee4369adad29c3174aab237d2ad4ca7b54
John Roca <rocajohntemoty@gmail.com>
2018-03-05 16:58:56 +0800

    Model Seeder

--- database/factories/ModelFactory.php
+++ database/factories/ModelFactory.php
@@ -15,7 +15,46 @@ $factory->define(App\Models\User::class, function (Faker\Generator $faker) {
     return [
         'name' => $faker->name,
         'email' => $faker->safeEmail,
-        'password' => bcrypt(str_random(10)),
+        'role'	=> rand(1,2),
+        'password' => bcrypt('password'),
         'remember_token' => str_random(10),
+    ];
+});
+
+$factory->define(App\Models\Teacher::class, function (Faker\Generator $faker) {
+    return [
+        'name' => $faker->name,
+        'email' => $faker->safeEmail,
+        'role'	=> 1,
+        'password' => bcrypt('password'),
+        'remember_token' => str_random(10),
+    ];
+});
+
+$factory->define(App\Models\Student::class, function (Faker\Generator $faker) {
+    return [
+        'name' => $faker->name,
+        'email' => $faker->safeEmail,
+        'role'	=> 2,
+        'password' => bcrypt('password'),
+        'remember_token' => str_random(10),
+    ];
+});
+
+$factory->define(App\Models\Subject::class, function (Faker\Generator $faker) {
+	$teacher = factory(App\Models\Teacher::class)->create();
+
+    return [
+        'name' => $faker->words(3, true),
+        'teacher_id' => $teacher->id
+    ];
+});
+
+$factory->define(App\Models\Quiz::class, function (Faker\Generator $faker) {
+	$subject = factory(App\Models\Subject::class)->create();
+
+    return [
+        'title' => $faker->words(5, true),
+        'subject_id' => $subject->id
     ];
 });
