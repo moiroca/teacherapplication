7533249435c162295c9b7cf24ab16e03eab5a1d4
John Roca <rocajohntemoty@gmail.com>
2018-03-11 18:53:23 +0800

    Smaller Login Title

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -25,7 +25,7 @@
             <section class="login_content">
 				{!! BootForm::open(['url' => url('/login'), 'method' => 'post']) !!}
                     
-				<h3>{{ config('app.name') }} Login</h3>
+				<h4>{{ config('app.name') }} Login</h4>
 			
 				{!! BootForm::email('email', 'Email', old('email'), ['placeholder' => 'Email', 'afterInput' => '<span>test</span>'] ) !!}
 			
