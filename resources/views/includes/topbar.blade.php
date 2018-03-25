7757bdc65acf818f3d7cdf5af3fd1736c36f7ae6
John Roca <rocajohntemoty@gmail.com>
2018-03-25 17:32:39 +0800

    Fix Profile

--- resources/views/includes/topbar.blade.php
+++ resources/views/includes/topbar.blade.php
@@ -9,11 +9,16 @@
             <ul class="nav navbar-nav navbar-right">
                 <li class="">
                     <a href="javascript:;" class="user-profile dropdown-toggle" data-toggle="dropdown" aria-expanded="false">
-                        <img src="#" alt="">
+                        <!-- <img src="#" alt=""> -->
                         {{ Auth::user()->name }}
                         <span class=" fa fa-angle-down"></span>
                     </a>
                     <ul class="dropdown-menu dropdown-usermenu pull-right">
+                        @if(\Auth::user()->isTeacher())
+                            <li>
+                                <a href="#">Profile</a>
+                            </li>
+                        @endif
                         <li><a href="{{ url('/logout') }}"><i class="fa fa-sign-out pull-right"></i> Log Out</a></li>
                     </ul>
                 </li>
